6beaf05cf23b20c7706f5fef96ba3bdb94c4939f
Gustavo Medeiros <cipguga@gmail.com>
2025-11-26 08:33:17 -0300

    1o commit

---
+++ src/main/java/com/example/WebApp/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.WebApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public @ResponseBody String greeting() {
+		
+		return "Hello, World!";
+	}
+	
+	@RequestMapping("/Gustavo")
+	public @ResponseBody String greetingGustavo() {
+	return "Olá, Gustavo Medeiros!";
+	}
+}
